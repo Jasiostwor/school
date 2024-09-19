37cc26722a6ffb314f58e1faeb34f1fcca566d63
Jasiostwor <j.horodecki@game-changer.pl>
2024-09-19 11:41:54 +0200

    Lesson

---
+++ Zawy/Person/PersonApp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from models import Grupa, Person
+
+# Register your models here.
+
+admin.site.register(Grupa)
+admin.site.register(Person)
---
+++ Zawy/Person/PersonApp/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class Grupa(models.Model):
+    nazwa = models.CharField(max_length=100)
+
+    # def __str__(self):
+    #     return self.nazwa
+
+class Person(models.Model):
+    nazwisko = models.CharField(max_length=100)
+    imie = models.CharField(max_length=40)
+    stanowisko = models.CharField(max_length=50)
+    grupa = models.ForeignKey(Grupa, on_delete=models.SET_NULL, null=True)
+    # def __str__(self):
+    #     return self.nazwa
