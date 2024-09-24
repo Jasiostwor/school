2968190929ffaeab48eb352b12443dddaa6d2281
Jasiostwor <j.horodecki@game-changer.pl>
2024-09-24 09:07:45 +0200

    Filipiak tu byl

--- Desktopy/FirstApp/demo.py
+++ Desktopy/FirstApp/demo.py
@@ -9,7 +9,17 @@ class demo(QWidget):
 
         loader = QUiLoader()
         self.win = loader.load("dapp.ui", None)
+
+        self.win.pushButton.clicked.connect(self.clear_text)  # Wyłącz aplikację
+        self.win.pushButton_2.clicked.connect(self.close_app)  # Wyczyść pole tekstowe
+
         self.win.show()
+
+    def close_app(self):
+        QApplication.quit()
+
+    def clear_text(self):
+        self.win.lineEdit.clear()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
